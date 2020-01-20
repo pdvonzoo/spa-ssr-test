4d2dcc17161b477153fa9ca7704015b1d02b7bc4
윤상현 <saga@wemakeprice.com>
2020-01-20 10:28:10 +0900

    [NEWBIE1-44] feat: 서버사이드 렌더링 에러 핸들링

--- server/index.js
+++ server/index.js
@@ -2,13 +2,16 @@ import express from "express"
 import React from "react"
 import { renderToString } from "react-dom/server"
 import { StaticRouter } from "react-router-dom"
-
 import { Provider } from "react-redux";
 import createSagaMiddleware from "redux-saga";
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../src/modules";
 import rootSaga from "../src/sagas";
 import App from "../src/App"
+import path from 'path'
+import fs from 'fs';
+import { ServerStyleSheet } from 'styled-components'
+
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
@@ -21,35 +24,30 @@ const app = express()
 
 app.use(express.static("dist"))
 
+const preloadedState = store.getState()
+
+app.get('/favicon.ico', (req, res) => res.sendStatus(204));
 app.get("*", (req, res, next) => {
-    const markup = renderToString(
+
+    const sheet = new ServerStyleSheet();
+    const renderString = renderToString(sheet.collectStyles(
         <Provider store={store}>
             <StaticRouter location={req.url}>
                 <App />
             </StaticRouter>
-        </Provider>
+        </Provider>)
     )
+    const styles = sheet.getStyleTags();
+    const html = fs.readFileSync('./template/index.html', 'utf8');
 
-    const preloadedState = store.getState()
 
-    res.send(`
-        <!DOCTYPE html>
-        <html>
-            <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>도서대여 프로젝트</title>
-            </head>
-            <body>
-            <script>
-                window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
-            </script>
-            <div id="root">${markup}</div>
-            <script src="/bundle.js"></script>
-            </body>
-        </html>
-        `)
+    const result = html
+        .replace(`<div id="root"></div>`, `<div id="root">${renderString}</div>`)
+        .replace('__DATA_FROM_SERVER__', `${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}`)
+        .replace('__STYLE_STATE__', styles)
+
+    res.send(result)
+
 })
 
 app.listen(3000, () => {
